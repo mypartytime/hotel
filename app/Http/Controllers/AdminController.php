2db2f224541c3513d8640b07d8629a80ec229036
mypartytime <140714609+mypartytime@users.noreply.github.com>
2023-09-18 15:54:45 +0700

    Admin page setup

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,12 +3,61 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AdminController extends Controller
 {
     public function AdminDashboard(){
 
-        return view('admin.admin_dashboard');
+        return view('admin.index');
 
     }//end method
+
+    public function AdminLogout(Request $request){
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
+    } // End Method 
+
+    public function AdminLogin(){
+
+        return view('admin.admin_login');
+
+    }// End Method 
+
+    public function AdminProfile(){
+
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_profile_view',compact('profileData'));
+
+    }// End Method 
+
+    public function AdminProfileStore(Request $request){
+
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+
+        if($request->file('photo')){
+            $file = $request->file('photo');
+            $filename = date('YmdHi').$file->getClientOriginalName();  
+            $file->move(public_path('upload/admin_images'),$filename);
+            $data['photo'] = $filename;
+
+        }
+        $data->save();
+
+        return redirect()->back();
+
+    }// End Method 
+
 }
