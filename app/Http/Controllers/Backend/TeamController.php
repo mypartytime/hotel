728f93b8af8970a0eb5124241fd471d950c5f43d
mypartytime <140714609+mypartytime@users.noreply.github.com>
2023-09-18 17:48:33 +0700

    team store

--- app/Http/Controllers/Backend/TeamController.php
+++ app/Http/Controllers/Backend/TeamController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Team;
+use Intervention\Image\Facades\Image;
+use Carbon\Carbon;
 
 class TeamController extends Controller
 {
@@ -14,4 +16,33 @@ class TeamController extends Controller
         return view('backend.team.all_team',compact('team'));
 
     } // end method
+
+    public function AddTeam(){
+        return view('backend.team.add_team');
+    } // End Method 
+
+    public function StoreTeam(Request $request){
+
+        $image = $request->file('image');
+        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+        Image::make($image)->resize(550,670)->save('upload/team/'.$name_gen);
+        $save_url = 'upload/team/'.$name_gen;
+
+        Team::insert([
+
+            'name' => $request->name,
+            'postion' => $request->postion,
+            'facebook' => $request->facebook,
+            'image' => $save_url,
+            'created_at' => Carbon::now(),
+        ]);
+
+        $notification = array(
+            'message' => 'Team Data Inserted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.team')->with($notification);
+
+    } // End Method 
 }
--- resources/views/backend/team/all_team.blade.php
+++ resources/views/backend/team/all_team.blade.php
@@ -4,15 +4,16 @@
 <div class="page-content">
     <!--breadcrumb-->
     <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
-
+        
         <div class="ps-3">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb mb-0 p-0">
-                    <button type="button" class="btn btn-outline-primary px-5 radius-30">Add Team </button>
+                   
+                    <a href="{{ route('add.team') }}" class="btn btn-outline-primary px-5 radius-30"> Add Team</a>
                 </ol>
             </nav>
         </div>
-
+        
     </div>
     <!--end breadcrumb-->
     <h6 class="mb-0 text-uppercase">All Team</h6>
@@ -23,33 +24,39 @@
                 <table id="example" class="table table-striped table-bordered" style="width:100%">
                     <thead>
                         <tr>
+                            <th>Sl</th>
+                            <th>Image</th>
                             <th>Name</th>
-                            <th>Position</th>
-                            <th>Office</th>
-                            <th>Age</th>
-                            <th>Start date</th>
-                            <th>Salary</th>
+                            <th>Postion</th>
+                            <th>Facebook</th>
+                            <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
+                       @foreach ($team as $key=> $item ) 
                         <tr>
-                            <td>Tiger Nixon</td>
-                            <td>System Architect</td>
-                            <td>Edinburgh</td>
-                            <td>61</td>
-                            <td>2011/04/25</td>
-                            <td>$320,800</td>
+                            <td>{{ $key+1 }}</td>
+                            <td> <img src="{{ asset($item->image) }}" alt="" style="width:70px; height:40px;" > </td>
+                            <td>{{ $item->name }}</td>
+                            <td>{{ $item->postion }}</td>
+                            <td>{{ $item->facebook }}</td>
+                            <td>
+    <a href="" class="btn btn-warning px-3 radius-30"> Edit</a>
+    <a href="" class="btn btn-danger px-3 radius-30"> Delete</a>
+
+                            </td>
                         </tr>
-
+                        @endforeach 
+                      
                     </tbody>
-
+                 
                 </table>
             </div>
         </div>
     </div>
-
+     
     <hr/>
-
+     
 </div>
 
 
