2f75b4c337a8de2daa5921fc14c1d3c82a80faa9
mypartytime <140714609+mypartytime@users.noreply.github.com>
2023-09-19 13:19:06 +0700

    Add room number in main room type

--- app/Http/Controllers/Backend/RoomController.php
+++ app/Http/Controllers/Backend/RoomController.php
@@ -9,6 +9,7 @@ use Intervention\Image\Facades\Image;
 use Carbon\Carbon;
 use App\Models\Facility;
 use App\Models\MultiImage;
+use App\Models\RoomNumber;
 
 class RoomController extends Controller
 {
@@ -17,7 +18,8 @@ class RoomController extends Controller
         $basic_facility = Facility::where('rooms_id',$id)->get();
         $multiimgs = MultiImage::where('rooms_id',$id)->get();
         $editData = Room::find($id);
-        return view('backend.allroom.rooms.edit_rooms',compact('editData','basic_facility','multiimgs'));
+        $allroomNo = RoomNumber::where('rooms_id',$id)->get();
+        return view('backend.allroom.rooms.edit_rooms',compact('editData','basic_facility','multiimgs','allroomNo'));
 
     } //End Method 
 
@@ -133,4 +135,59 @@ class RoomController extends Controller
         return redirect()->back()->with($notification); 
 
     }//End Method 
+
+    public function StoreRoomNumber(Request $request,$id){
+
+        $data = new RoomNumber();
+        $data->rooms_id = $id;
+        $data->room_type_id = $request->room_type_id;
+        $data->room_no = $request->room_no;
+        $data->status = $request->status;
+        $data->save();
+
+        $notification = array(
+            'message' => 'Room Number Added Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification); 
+
+    }//End Method 
+
+    public function EditRoomNumber($id){
+
+        $editroomno = RoomNumber::find($id);
+        return view('backend.allroom.rooms.edit_room_no',compact('editroomno'));
+
+    }//End Method 
+
+    public function UpdateRoomNumber(Request $request, $id){
+
+        $data = RoomNumber::find($id);
+        $data->room_no = $request->room_no;
+        $data->status = $request->status;
+        $data->save();
+
+       $notification = array(
+            'message' => 'Room Number Updated Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('room.type.list')->with($notification); 
+
+    }//End Method 
+
+
+    public function DeleteRoomNumber($id){
+
+        RoomNumber::find($id)->delete();
+
+        $notification = array(
+            'message' => 'Room Number Deleted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('room.type.list')->with($notification); 
+
+    }//End Method
 }
--- routes/web.php
+++ routes/web.php
@@ -89,6 +89,11 @@ Route::controller(RoomController::class)->group(function(){
     Route::post('/update/room/{id}', 'UpdateRoom')->name('update.room');
     Route::get('/multi/image/delete/{id}', 'MultiImageDelete')->name('multi.image.delete');
 
+    Route::post('/store/room/no/{id}', 'StoreRoomNumber')->name('store.room.no');
+    Route::get('/edit/roomno/{id}', 'EditRoomNumber')->name('edit.roomno');
+    Route::post('/update/roomno/{id}', 'UpdateRoomNumber')->name('update.roomno');
+    Route::get('/delete/roomno/{id}', 'DeleteRoomNumber')->name('delete.roomno');
+
 });
 
 
