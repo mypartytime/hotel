edc3289442c5322f4496ede45a78f2ebe20b27b3
mypartytime <140714609+mypartytime@users.noreply.github.com>
2023-09-19 16:55:49 +0700

    Delete room

--- app/Http/Controllers/Backend/RoomController.php
+++ app/Http/Controllers/Backend/RoomController.php
@@ -10,6 +10,7 @@ use Carbon\Carbon;
 use App\Models\Facility;
 use App\Models\MultiImage;
 use App\Models\RoomNumber;
+use App\Models\RoomType;
 
 class RoomController extends Controller
 {
@@ -188,6 +189,37 @@ class RoomController extends Controller
         );
 
         return redirect()->route('room.type.list')->with($notification); 
+
+    }//End Method
+
+    public function DeleteRoom(Request $request, $id){
+        $room = Room::find($id);
+
+        if (file_exists('upload/roomimg/'.$room->image) AND ! empty($room->image)) {
+           @unlink('upload/roomimg/'.$room->image);
+        }
+
+        $subimage = MultiImage::where('rooms_id',$room->id)->get()->toArray();
+        if (!empty($subimage)) {
+            foreach ($subimage as $value) {
+               if (!empty($value)) {
+               @unlink('upload/roomimg/multi_img/'.$value['multi_img']);
+               }
+            }
+        }
+
+        RoomType::where('id',$room->roomtype_id)->delete();
+        MultiImage::where('rooms_id',$room->id)->delete();
+        Facility::where('rooms_id',$room->id)->delete();
+        RoomNumber::where('rooms_id',$room->id)->delete();
+        $room->delete();
+
+        $notification = array(
+            'message' => 'Room Deleted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);  
 
     }//End Method
 }
