995b085c395a0c75b2481f0c184827cefe5a5960
mypartytime <140714609+mypartytime@users.noreply.github.com>
2023-09-26 13:03:22 +0700

    Room Checkout with COD

--- routes/web.php
+++ routes/web.php
@@ -121,6 +121,7 @@ Route::controller(BookingController::class)->group(function(){
 
    Route::get('/checkout/', 'Checkout')->name('checkout');
    Route::post('/booking/store/', 'BookingStore')->name('user_booking_store');
+   Route::post('/checkout/store/', 'CheckoutStore')->name('checkout.store');
 
 
 });
