60432268066639ea85b06852bc1e128764941d2a
mypartytime <140714609+mypartytime@users.noreply.github.com>
2023-09-28 16:55:12 +0700

    Sale report

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use App\Http\Controllers\Backend\SettingController;
 use App\Http\Controllers\Backend\TestimonialController;
 use App\Http\Controllers\Backend\BlogController;
 use App\Http\Controllers\Backend\CommentController;
+use App\Http\Controllers\Backend\ReportController;
 
  
 
@@ -211,6 +212,13 @@ Route::controller(BookingController::class)->group(function(){
     Route::controller(CommentController::class)->group(function(){ 
         Route::get('/all/comment/', 'AllComment')->name('all.comment'); 
         Route::post('/update/comment/status', 'UpdateCommentStatus')->name('update.comment.status');
+    });
+
+    /// Booking Report All Route 
+    Route::controller(ReportController::class)->group(function(){ 
+        Route::get('/booking/report/', 'BookingReport')->name('booking.report');
+        Route::post('/search-by-date', 'SearchByDate')->name('search-by-date');
+
     });
 
 });
