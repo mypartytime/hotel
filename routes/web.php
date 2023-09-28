56bd553272773ca7a72fcd700605570144d1f712
mypartytime <140714609+mypartytime@users.noreply.github.com>
2023-09-28 15:51:36 +0700

    Blog post in admin area

--- routes/web.php
+++ routes/web.php
@@ -168,6 +168,19 @@ Route::controller(BlogController::class)->group(function(){
 
 });
 
+/// Blog Post All Route 
+Route::controller(BlogController::class)->group(function(){
+
+    Route::get('/all/blog/post', 'AllBlogPost')->name('all.blog.post');
+    Route::get('/add/blog/post', 'AddBlogPost')->name('add.blog.post');
+    Route::post('/store/blog/post', 'StoreBlogPost')->name('store.blog.post');
+    Route::get('/edit/blog/post/{id}', 'EditBlogPost')->name('edit.blog.post');
+    Route::post('/update/blog/post', 'UpdateBlogPost')->name('update.blog.post');
+    Route::get('/delete/blog/post/{id}', 'DeleteBlogPost')->name('delete.blog.post');
+
+
+});
+
 // Auth Middleware User must have login for access this route 
 Route::middleware(['auth'])->group(function(){
 
